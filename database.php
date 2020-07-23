06a3a893d600843a53e18c8b98379c7790a0f1b7
Mahavishnu <selvamvishnu25@gmail.com>
2020-07-23 16:51:07 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$db=new mysqli("localhost","root","","e-library");
+$db=new mysqli("us-cdbr-east-02.cleardb.com","b216c21115e491","bdb8f51c","heroku_e8a5c2dd93d719b");
 if(!$db)
 {
 	echo"connection failed";
